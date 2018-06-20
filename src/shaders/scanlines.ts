12f79f09bc0c17cd024a7239282606e234a2d0e6
David Lee <david.isaac.lee@gmail.com>
2018-06-19 19:06:24 -0700

    Add dithering to scanlines module

--- src/shaders/scanlines.ts
+++ src/shaders/scanlines.ts
@@ -7,8 +7,21 @@ export default glsl`
 
 	uniform vec2 inputTextureDimensions;
 
+	float rand(vec2 n) { 
+		return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+	}
+
+	float noise(vec2 n) {
+		const vec2 d = vec2(0.0, 1.0);
+		vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+		return mix(
+			mix(rand(b), rand(b + d.yx), f.x),
+			mix(rand(b + d.xy), rand(b + d.yy), f.x),
+			f.y);
+	}
+
 	void main() {
-		highp vec2 uv =
+		vec2 uv =
 			gl_FragCoord.xy / inputTextureDimensions;
 
 		gl_FragColor = vec4(
@@ -17,5 +30,7 @@ export default glsl`
 				+ (uv.y - mod(uv.y, 1. / N_SCANLINES))
 			),
 			1.);
+		gl_FragColor.rgb += mix(-0.5/128., 0.5/128., noise(gl_FragCoord.xy));
 	}
+
 `;
