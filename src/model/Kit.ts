da481232871a9772fc4327cf52f4b56406012172
David Lee <david.isaac.lee@gmail.com>
2018-06-21 00:22:30 -0700

    Remove outdated comments

--- src/model/Kit.ts
+++ src/model/Kit.ts
@@ -9,12 +9,10 @@ import { proOsc } from './modules/pro-osc';
 import { identity } from './modules/identity';
 import { VideoModule } from './VideoModule';
 
-// TODO: Would be nice to make this typesafe with Kit.modules
 export type ModuleType =
 	"identity" | "oscillator" | "constant" | "mixer" | "scanlines"
 	| "pro-osc" | "crosshatch" | "dither" | "rgbOffset";
 
-// key :: ModuleType
 export const modules: Record<ModuleType, VideoModule> = {
 	rgbOffset,
 	dither,
