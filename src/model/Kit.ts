54f182beab445c21ab326163e74fdd3e0904d35d
David Lee <david.isaac.lee@gmail.com>
2018-06-23 10:01:35 -0700

    [WIP] Disable all modules except oscillator

--- src/model/Kit.ts
+++ src/model/Kit.ts
@@ -1,32 +1,43 @@
+/*
 import { scanlines } from './modules/scanlines';
 import { constant } from './modules/constant';
 import { mixer } from './modules/mixer';
 import { crosshatch } from './modules/crosshatch';
 import { dither } from './modules/dither';
 import { rgbOffset } from './modules/rgbOffset';
+*/
 import { oscillator } from './modules/oscillator';
+/*
 import { identity } from './modules/identity';
 import { addFract } from './modules/addFract';
 import { divide } from './modules/divide';
 import { phaseDelta } from './modules/phaseDelta';
+*/
 import { VideoModule } from './VideoModule';
 
 export type ModuleType =
+	"oscillator";
+	/*
 	"identity" | "oscillator" | "constant" | "mixer" | "scanlines"
 	| "crosshatch" | "dither" | "rgbOffset" | "addFract"
 	| "divide" | "phaseDelta";
+	*/
 
 export const modules: Record<ModuleType, VideoModule> = {
+	/*
 	rgbOffset,
 	dither,
 	crosshatch,
 	identity,
+	*/
 	oscillator,
+	/*
 	constant,
 	mixer,
 	scanlines,
 	addFract,
 	divide,
 	phaseDelta,
+	*/
 };
 
