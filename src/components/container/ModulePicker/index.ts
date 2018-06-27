8df7dc1f52451fe8fa3673c6284a99b7b5b8c756
David Lee <david.isaac.lee@gmail.com>
2018-06-27 12:56:43 -0700

    Style module picker

--- src/components/container/ModulePicker/index.ts
+++ src/components/container/ModulePicker/index.ts
@@ -1,19 +1,49 @@
 import * as React from 'react';
+import styled from '../../../styled-components';
 import * as Kit from '../../../model/Kit';
 
 const e = React.createElement;
+
+const ModuleList = styled.ul`
+	margin: 0;
+	padding: 0;
+	
+	display: grid;
+
+	grid-template-columns: 50% 50%;
+`;
+
+const ModuleButton = styled.button`
+	background: none;
+	border: none;
+
+	width: 100%;
+	height: 100%;
+	margin: 0;
+	padding: 20px 0;
+
+	background-color: black;
+	border: 2px solid white;
+	border-radius: 4px;
+	color: white;
+`;
+
+const ModuleEntry = styled.li`
+	list-style-type: none;
+	margin: 10px;
+`;
 
 interface Props {
 	addModule: (modType: Kit.ModuleType) => any;
 }
 
 const ModulePicker: React.StatelessComponent<Props> = ({ addModule }) =>
-	e('ul',
+	e(ModuleList,
 		{},
 		...Kit.moduleKeys.map(key =>
-			e('li',
+			e(ModuleEntry,
 				{},
-				e('button',
+				e(ModuleButton,
 					{
 						onClick: () => addModule(key)
 					},
