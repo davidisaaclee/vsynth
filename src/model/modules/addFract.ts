d93b883b8d4cc01031421407f458e4c0825d8fe0
David Lee <david.isaac.lee@gmail.com>
2018-06-28 17:22:45 -0700

    Add parameter scaling controls for addFract module

--- src/model/modules/addFract.ts
+++ src/model/modules/addFract.ts
@@ -1,6 +1,11 @@
 import { glsl } from '@davidisaaclee/video-graph';
 import { VideoModule, ShaderModule } from '../VideoModule';
 
+export const parameterKeys = {
+	'a': 'a',
+	'b': 'b',
+};
+
 export const inletKeys = {
 	'a': 'a',
 	'b': 'b',
@@ -10,15 +15,19 @@ const shaderSource = glsl`
 	precision mediump float;
 
 	uniform vec2 inputTextureDimensions;
+
+	uniform float leftAmount;
 	uniform sampler2D leftTexture;
+
+	uniform float rightAmount;
 	uniform sampler2D rightTexture;
 
 	void main() {
 		vec2 samplePoint =
 			gl_FragCoord.xy / inputTextureDimensions;
 		gl_FragColor =
-			texture2D(leftTexture, samplePoint)
-			+ texture2D(rightTexture, samplePoint);
+			texture2D(leftTexture, samplePoint) * leftAmount
+			+ texture2D(rightTexture, samplePoint) * rightAmount;
 		gl_FragColor.rgb = fract(gl_FragColor.rgb);
 	}
 `;
@@ -26,13 +35,22 @@ const shaderSource = glsl`
 // Adds two textures together, and takes the fractional component of the result.
 export const addFract: VideoModule<ShaderModule> = {
 	parameters: {
-		keys: [],
-		defaultValues: {},
+		keys: [
+			parameterKeys.a,
+			parameterKeys.b,
+		],
+		defaultValues: {
+			[parameterKeys.a]: 1,
+			[parameterKeys.b]: 1,
+		},
 	},
 
 	inlets: {
 		keys: [inletKeys.a, inletKeys.b],
-		associatedParameters: {},
+		associatedParameters: {
+			[inletKeys.a]: [parameterKeys.a],
+			[inletKeys.b]: [parameterKeys.b],
+		},
 	},
 
 	details: {
@@ -47,7 +65,16 @@ export const addFract: VideoModule<ShaderModule> = {
 			},
 		}),
 
-		parametersToUniforms: () => ({}),
+		parametersToUniforms: (values) => ({
+			'leftAmount': {
+				type: 'f',
+				data: values[parameterKeys.a],
+			},
+			'rightAmount': {
+				type: 'f',
+				data: values[parameterKeys.b],
+			},
+		}),
 
 		inletsToUniforms: {
 			[inletKeys.a]: 'leftTexture',
