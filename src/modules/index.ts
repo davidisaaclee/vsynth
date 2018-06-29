9947832091e7b2449579ce41f1a58dc1161586f4
David Lee <david.isaac.lee@gmail.com>
2018-06-28 22:26:34 -0700

    Filter transient preview parameter states from undo history

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { ActionType } from 'typesafe-actions';
 import undoable, { StateWithHistory } from 'redux-undo';
+import * as GraphConstants from './graph/constants';
 import * as Graph from './graph';
 import * as App from './app';
 
@@ -12,7 +13,16 @@ export interface State {
 type Action = ActionType<typeof Graph.actions | typeof App.actions>;
 
 export const reducer = combineReducers<State, Action>({
-	graph: undoable(Graph.reducer),
+	graph: undoable(Graph.reducer, {
+		filter: (action) => {
+			switch (action.type) {
+				case GraphConstants.PREVIEW_PARAMETER:
+					return false;
+				default:
+					return true;
+			}
+		}
+	}),
 	app: App.reducer,
 });
 
