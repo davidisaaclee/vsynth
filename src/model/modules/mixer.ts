a87a816ae9547efe31a0a19353cebdeddaf639fe
David Lee <david.isaac.lee@gmail.com>
2018-06-28 17:36:40 -0700

    Make mixer texture-controlled

--- src/model/modules/mixer.ts
+++ src/model/modules/mixer.ts
@@ -1,26 +1,75 @@
+import { glsl } from '@davidisaaclee/video-graph';
 import { VideoModule, ShaderModule } from '../VideoModule';
-import shaderSource from '../../shaders/mixer';
+
+const shaderSource = glsl`
+	precision mediump float;
+
+	uniform sampler2D mix;
+	uniform float mixAmount;
+
+	uniform vec2 inputTextureDimensions;
+
+	uniform sampler2D inputA;
+	uniform float inputAAmount;
+	uniform sampler2D inputB;
+	uniform float inputBAmount;
+
+	float maxComponent(vec3 v) {
+		return max(v.x, max(v.y, v.z));
+	}
+
+	void main() {
+		vec2 textureSamplePoint =
+			gl_FragCoord.xy / inputTextureDimensions;
+
+		float sampledMix =
+			maxComponent(texture2D(mix, textureSamplePoint).rgb * mixAmount);
+
+		vec4 a =
+			(texture2D(inputA, textureSamplePoint) * inputAAmount)
+			* vec4(smoothstep(0., 0.5, sampledMix));
+		vec4 b =
+			(texture2D(inputB, textureSamplePoint) * inputBAmount)
+			* vec4(smoothstep(0., 0.5, 1. - sampledMix));
+
+		gl_FragColor =
+			a + b;
+	}
+`;
 
 export const parameterKeys = {
 	mixAmount: 'mix amount',
+	a: 'a',
+	b: 'b',
 };
 
 export const inletKeys = {
+	mix: 'mix',
 	a: 'a',
 	b: 'b',
 };
 
 export const mixer: VideoModule<ShaderModule> = {
 	parameters: {
-		keys: [parameterKeys.mixAmount],
+		keys: [
+			parameterKeys.mixAmount,
+			parameterKeys.a,
+			parameterKeys.b,
+		],
 		defaultValues: {
 			[parameterKeys.mixAmount]: 0.5,
+			[parameterKeys.a]: 1,
+			[parameterKeys.b]: 1,
 		},
 	},
 
 	inlets: {
-		keys: [inletKeys.a, inletKeys.b],
-		associatedParameters: {},
+		keys: [inletKeys.mix, inletKeys.a, inletKeys.b],
+		associatedParameters: {
+			[inletKeys.mix]: [parameterKeys.mixAmount],
+			[inletKeys.a]: [parameterKeys.a],
+			[inletKeys.b]: [parameterKeys.b],
+		},
 	},
 
 	details: {
@@ -34,10 +83,19 @@ export const mixer: VideoModule<ShaderModule> = {
 			'mixAmount': {
 				type: 'f',
 				data: values[parameterKeys.mixAmount]
-			}
+			},
+			'inputAAmount': {
+				type: 'f',
+				data: values[parameterKeys.a]
+			},
+			'inputBAmount': {
+				type: 'f',
+				data: values[parameterKeys.b]
+			},
 		}),
 
 		inletsToUniforms: {
+			[inletKeys.mix]: 'mix',
 			[inletKeys.a]: 'inputA',
 			[inletKeys.b]: 'inputB',
 		}
