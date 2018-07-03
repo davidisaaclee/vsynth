07d01d214068094348b5f6058befb9376118ffe9
David Lee <david.isaac.lee@gmail.com>
2018-07-02 17:57:48 -0700

    Blacklist previewed parameter changes from redux-persist

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,34 @@
 import { combineReducers } from 'redux';
+import { createMigrate, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { ActionType } from 'typesafe-actions';
 import undoable, { StateWithHistory } from 'redux-undo';
 import * as GraphConstants from './graph/constants';
 import * as Graph from './graph';
 import * as App from './app';
+
+
+function clearPastStateMigration(state: any): any {
+	return {};
+}
+
+const migrations = {
+	3: clearPastStateMigration,
+	4: clearPastStateMigration,
+};
+const persistRootConfig = {
+  key: 'root',
+  storage,
+	version: 4,
+	migrate: createMigrate(migrations, { debug: process.env.NODE_ENV === 'development' }),
+	blacklist: ['graph'],
+};
+
+const persistGraphConfig = {
+	key: 'graph',
+	blacklist: ['previewedParameterChanges'],
+	storage
+};
 
 export interface State {
 	graph: StateWithHistory<Graph.State>;
@@ -12,17 +37,19 @@ export interface State {
 
 type Action = ActionType<typeof Graph.actions | typeof App.actions>;
 
-export const reducer = combineReducers<State, Action>({
-	graph: undoable(Graph.reducer, {
-		filter: (action) => {
-			switch (action.type) {
-				case GraphConstants.PREVIEW_PARAMETER:
-					return false;
-				default:
-					return true;
+export const reducer = persistReducer(
+	persistRootConfig,
+	combineReducers<State, Action>({
+		graph: persistReducer(persistGraphConfig, undoable(Graph.reducer, {
+			filter: (action) => {
+				switch (action.type) {
+					case GraphConstants.PREVIEW_PARAMETER:
+						return false;
+					default:
+						return true;
+				}
 			}
-		}
-	}),
-	app: App.reducer,
-});
+		})),
+		app: App.reducer,
+	}));
 
--- src/store.ts
+++ src/store.ts
@@ -1,37 +1,17 @@
 import { createStore } from 'redux';
-import { createMigrate, persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { persistStore } from 'redux-persist';
 
 import { reducer as rootReducer } from './modules';
 
 const devToolsKey = '__REDUX_DEVTOOLS_EXTENSION__';
 
-function clearPastStateMigration(state: any): any {
-	return {};
-}
-
-const migrations = {
-	3: clearPastStateMigration,
-	4: clearPastStateMigration,
-};
-
-const persistConfig = {
-  key: 'root',
-  storage,
-	version: 4,
-	migrate: createMigrate(migrations, { debug: process.env.NODE_ENV === 'development' })
-};
-
-const persistedReducer =
-	persistReducer(persistConfig, rootReducer);
-
 function configureStore(initialState?: object) {
   return createStore(
-		persistedReducer,
+		rootReducer,
 		window[devToolsKey] && window[devToolsKey]()
 	);
 }
 
-export const store = configureStore(persistedReducer);
+export const store = configureStore(rootReducer);
 export const persistor = persistStore(store);
 // persistor.purge();
