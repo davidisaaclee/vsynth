3d189e435cabdf7ccb6054d82112a63dc2629ced
David Lee <david.isaac.lee@gmail.com>
2018-07-05 14:47:30 -0700

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Editor from './components/container/Editor';
 import './App.css';
 
 const e = React.createElement;
 
-const App: React.StatelessComponent<object> = props => (
-	e(Editor)
-);
+const App: React.StatelessComponent<object> = () => (
+	e(Router,
+		{},
+		e('div', {},
+			e(Route,
+				{
+					exact: true,
+					path: '/',
+					component: Editor
+				}),
+			e(Route,
+				{
+					path: '/help',
+					render: () => e('span', {}, 'help')
+				}))));
 
 export default App;
 
