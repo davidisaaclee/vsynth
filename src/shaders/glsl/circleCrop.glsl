16df7feb7ea784c6724d96bf1e4280aaf4ccfead
David Lee <david.isaac.lee@gmail.com>
2018-08-08 07:58:05 -0700

    Feather edge of circle crop

--- src/shaders/glsl/circleCrop.glsl
+++ src/shaders/glsl/circleCrop.glsl
@@ -1,6 +1,8 @@
-precision mediump float;
+precision highp float;
 
 #pragma glslify: sampleTex = require('./sampleTex');
+
+const float RADIUS_MIN = 0.001;
 
 uniform vec2 inputTextureDimensions;
 
@@ -24,16 +26,17 @@ float sqrDistance(vec2 a, vec2 b) {
 
 float pointInsideEllipse(vec2 pt, vec2 origin, vec2 radius) {
 	vec2 numerators = (pt - origin) * (pt - origin);
-	vec2 sqrRadius = max(vec2(0.001), radius * radius);
-	return 1. - step(1., numerators.x / sqrRadius.x + numerators.y / sqrRadius.y);
+	vec2 sqrRadius = radius * radius;
+	float smoothDistance = 0.1;
+	return 1. - smoothstep(1. - smoothDistance, 1., numerators.x / sqrRadius.x + numerators.y / sqrRadius.y);
 }
 
 void main() {
 	vec2 uv = gl_FragCoord.xy / inputTextureDimensions;
 
-	float radius = sampleTex(radiusTexture, uv, radiusAmount);
+	float radius = max(RADIUS_MIN, sampleTex(radiusTexture, uv, radiusAmount));
 	vec2 aspectRatioRadius =
-		vec2(radius, inputTextureDimensions.x / inputTextureDimensions.y * radius);
+		vec2(radius, radius * inputTextureDimensions.x / inputTextureDimensions.y);
 	vec2 center = vec2(
 		sampleTex(centerXTexture, uv, centerXAmount),
 		sampleTex(centerYTexture, uv, centerYAmount)
@@ -41,4 +44,5 @@ void main() {
 
   vec4 texColor = texture2D(inputTexture, uv) * inputAmount * pointInsideEllipse(uv, center, aspectRatioRadius);
   gl_FragColor = texColor;
+	gl_FragColor *= 1. - step(radius, RADIUS_MIN);
 }
